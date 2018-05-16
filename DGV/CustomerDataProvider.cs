f07ad79eaf2955ac4908a15404bfec859c99871e
Mark Buttsworrh <mbuttsworth@hotmail.com>
2018-05-16 09:04:40 +0800

    Add project files.

---
+++ DGV/CustomerDataProvider.cs
@@ -0,0 +1,60 @@
+﻿using DGV.AdventureWorks2012CustomerDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGV
+{
+    public class CustomerDataProvider
+    {
+        private CustomerTableAdapter adapter;
+
+        private AdventureWorks2012CustomerDataSet dataset;
+
+        private List<string> StoreIds = new List<string>();
+
+        public CustomerDataProvider()
+        {
+            dataset = new AdventureWorks2012CustomerDataSet();
+            adapter = new CustomerTableAdapter();
+            adapter.Fill(dataset.Customer);
+            SqlCommand sqlcom = new SqlCommand("Select DISTINCT StoreID from AdventureWorks2012.Sales.Customer Order By StoreID", adapter.Connection);
+            adapter.Connection.Open();
+            SqlDataReader dr =  sqlcom.ExecuteReader();//dataset.Customer.CreateDataReader();
+            
+            while (dr.Read())
+            {                
+                string item = dr.GetValue(0).ToString();
+                StoreIds.Add(item);                
+            }
+
+            adapter.Connection.Close();
+        }
+
+        public List<string> getStoreIds()
+        {
+            return StoreIds;
+        }
+        public DataView GetCustomers()
+        {
+            return dataset.Customer.DefaultView;
+        }
+
+        public DataView GetCustomersForStore(string storeId)
+        {
+
+            SqlCommand sqlcom = new SqlCommand("Select * from AdventureWorks2012.Sales.Customer Where StoreId = " + storeId + " Order By CustomerID", adapter.Connection);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcom);
+            DataSet ds = new DataSet();
+
+            adapter.Connection.Open();
+            da.Fill(ds);
+            adapter.Connection.Close();
+            return ds.Tables[0].DefaultView;
+        }
+    }
+}
---
+++ DGV/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DGV.AdventureWorks2012CustomerDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DGV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomerDataProvider cdp = new CustomerDataProvider();
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+            DataContext = cdp.GetCustomers();
+            foreach(var s in cdp.getStoreIds())
+                StoreIdsCombo.Items.Add(s);
+        }
+
+        private void StoreIdsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var storeid = (string)StoreIdsCombo.SelectedItem;
+            DataContext = cdp.GetCustomersForStore(storeid);
+        }
+    }
+}
